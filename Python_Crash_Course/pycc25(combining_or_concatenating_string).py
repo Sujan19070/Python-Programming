991c67c0a5fd8ba34b1419d5e28f684d750e526f
Nabib Ahamed <sujanofficial19070@gmail.com>
2026-03-07 06:17:09 +0600

    first commit of python section

---
+++ Python_Crash_Course/pycc25(combining_or_concatenating_string).py
@@ -0,0 +1,7 @@
+first_name = "Nabib"
+last_name  = "Sujan"
+full_name  = first_name +" "+ last_name
+print(full_name)
+print("Hello,"+full_name.title()+"!")
+message = "Hello,"+full_name.title()+"!"
+print(message)
---
+++ Python_Crash_Course/pycc27(strippingWhitespace_rstrip).py
@@ -0,0 +1,8 @@
+favt_language = 'python '
+print(favt_language)
+print(favt_language.rstrip())
+language = ' Python '
+print(language)
+print(language.rstrip())
+print(language.lstrip())
+print(language.strip())
